dcd7fc4ca95f2b1b45aa0af4cef91c6c37e47884
jamcha <jamcha.aa@gmail.com>
2017-10-04 04:27:09 +0900

    Fix Navi2ch for 5ch

--- init.el
+++ init.el
@@ -581,6 +581,12 @@
 (if (eq system-type 'gnu/linux)
     (setq navi2ch-net-http-proxy "localhost:8080"))
 
+(setq navi2ch-list-valid-host-regexp
+(concat "\\("
+(regexp-opt '(".2ch.net" ".5ch.net" ".bbspink.com" ".machibbs.com" ".machi.to"))
+"\\)\\'"))
+(setq navi2ch-list-bbstable-url "http://menu.5ch.net/bbstable.html") 
+
 ;; magit
 (require 'magit)
 
